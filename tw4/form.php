ea39be4e39c6ae7412d5139ad85680e3defb9bb6
VladGelen <vladislavsirotin2004@gmail.com>
2024-05-05 21:40:41 +0300

    1

--- tw4/form.php
+++ tw4/form.php
@@ -11,7 +11,7 @@
     </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
-<body style="background-image: linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%);">
+<body>
 <?php
 if (!empty($messages)) {
     print('<div id="messages">');
